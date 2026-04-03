13d6bab4eb70713ca15b2e9d14cd92aeb93a8a16
Manya Rawat <manyarawat2005@gmail.com>
2026-04-03 22:53:44 +0530

    config changed

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-
+console.log(" DATABASE_URL:", process.env.DATABASE_URL);
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
